63b6d34e02e9705bf9d7c85561f8318760f8b5c3
Mustafa Can <mustafaa.can.3535@gmail.com>
2026-02-20 01:06:40 +0300

    First Project

---
+++ firstproject/src/App.js
@@ -0,0 +1,28 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="text-center text-primary bg-danger diplay-4 px-3">
+          Hello World, this is the first edit
+        </p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
